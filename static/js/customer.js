7f76c74b34660d54e02906cac0c0ebff9034d642
hoangthach.dev <nguyenhoangthach2005@gmail.com>
2024-11-10 16:30:51 +0700

    them chuc nang sort o giao dien

--- static/js/customer.js
+++ static/js/customer.js
@@ -1,5 +1,7 @@
 formSearch = document.querySelector('.form-Search');
 
+let sortElement = document.querySelector('.btn-sort'); // Giả sử phần tử sort có class là 'sort-element'
+let sortAscending = true; // Biến để theo dõi trạng thái hiện tại
 if (formSearch) {
     formSearch.addEventListener('submit', function (e) {
         e.preventDefault();
@@ -16,3 +18,18 @@ if (formSearch) {
         }
     });
 }
+if (sortElement) {
+    sortElement.addEventListener('click', function () {
+        if (sortElement) {
+            if (sortAscending) {
+                sortElement.name = 'asc'; // Thay đổi giá trị của thuộc tính name thành tăng dần
+                sortAscending = false; // Chuyển trạng thái sang giảm dần
+            } else {
+                sortElement.name = 'desc'; // Thay đổi giá trị của thuộc tính name thành giảm dần
+                sortAscending = true; // Chuyển trạng thái sang tăng dần
+            }
+            window.location.href = '/sort?sort=' + sortElement.name; // Chuyển hướng trang với tham số sort
+        }
+    });
+}
+
