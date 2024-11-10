8c7e245175072a617bb149e0a708369a03f9ffb9
hoangthach.dev <nguyenhoangthach2005@gmail.com>
2024-11-10 11:11:42 +0700

    them database cho customer

--- blueprint/customer/customer_controller.py
+++ blueprint/customer/customer_controller.py
@@ -1,5 +1,15 @@
-from flask import render_template, request, current_app, url_for
-from flask import jsonify
+from flask import render_template, request, current_app
 import pandas as pd
+
 def index():
-    return render_template('customer.html')
+    mongo = current_app.config['MONGO']
+    collection = mongo.db.users  # Sử dụng cú pháp dấu chấm để truy cập collection
+    # Bước 1: Lấy dữ liệu từ MongoDB
+    data = list(collection.find())  # Trả về tất cả dữ liệu từ collection
+    
+    # Bước 2: Chuyển đổi đối tượng ObjectId thành chuỗi (nếu có)
+    for item in data:
+        item['_id'] = str(item['_id'])
+    
+    # Bước 3: Trả về dữ liệu dưới dạng JSON
+    return render_template('customer.html', records=data)
