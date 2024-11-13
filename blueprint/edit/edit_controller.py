d5be0da0e06cc4ca4a1d8f51fe7e8ae758d5471f
hoangthach.dev <nguyenhoangthach2005@gmail.com>
2024-11-13 22:58:18 +0700

    fix edit

--- blueprint/edit/edit_controller.py
+++ blueprint/edit/edit_controller.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, current_app, url_for
+from flask import render_template, request, current_app, url_for, redirect
 import pandas as pd
 from bson.objectid import ObjectId
 
@@ -12,21 +12,18 @@ def edit_P(ids):
 
     mongo = current_app.config['MONGO']
     collection = mongo.db.users
-    page = int(request.args.get('page', 1))
-    limit = 20
-    skip = (page - 1) * limit 
     data = {
-        "name": request.form.get("name"), 
-        "idcustomer": request.form.get("idcustomer"),
-        "segment": request.form.get("segment"),
-        "city": request.form.get("city"),
-        "state": request.form.get("state"),
-        "quantity": request.form.get("quantity"),
-        "region" : request.form.get("region")
+        "Name": request.form.get("name"), 
+        "IDCustomer": request.form.get("idcustomer"),
+        "Segment": request.form.get("segment"),
+        "City": request.form.get("city"),
+        "State": request.form.get("state"),
+        "Quantity": int(request.form.get("quantity")),
+        "Region" : request.form.get("region")
     }
     collection.update_one(
         {'_id': ObjectId(ids)},
         {'$set': data}  
     )
 
-    return data
+    return redirect(url_for('customer.home_route'))
