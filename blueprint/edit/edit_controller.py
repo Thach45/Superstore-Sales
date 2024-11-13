d13f89a8e05bdaa67bf77e8968af96c46db5fb28
ltaamlee <letam41225@gmail.com>
2024-11-13 22:33:23 +0700

    add edituser

--- blueprint/edit/edit_controller.py
+++ blueprint/edit/edit_controller.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, current_app
+from flask import render_template, request, current_app, url_for
 import pandas as pd
 from bson.objectid import ObjectId
 
@@ -10,6 +10,23 @@ def index(id):
 
 def edit_P(ids):
 
-    #Viết code ở đây 
-    data = request.form # đã lấy dữ liệu của request chạy thử để xem dữ liệu đã truyền qua chưa
+    mongo = current_app.config['MONGO']
+    collection = mongo.db.users
+    page = int(request.args.get('page', 1))
+    limit = 20
+    skip = (page - 1) * limit 
+    data = {
+        "name": request.form.get("name"), 
+        "idcustomer": request.form.get("idcustomer"),
+        "segment": request.form.get("segment"),
+        "city": request.form.get("city"),
+        "state": request.form.get("state"),
+        "quantity": request.form.get("quantity"),
+        "region" : request.form.get("region")
+    }
+    collection.update_one(
+        {'_id': ObjectId(ids)},
+        {'$set': data}  
+    )
+
     return data
