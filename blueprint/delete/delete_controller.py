2d87d7bc0888b241b9c100d60f02519dc369adb6
anhhiii <thutrangyuri510@gmail.com>
2024-11-16 00:41:35 +0700

    Fix delete function for customer

--- blueprint/delete/delete_controller.py
+++ blueprint/delete/delete_controller.py
@@ -1,7 +1,24 @@
 from flask import render_template, request, current_app, redirect, url_for
 from bson import ObjectId  # Để xử lý ObjectId của MongoDB
+from helper.infoTopCustomer import countUser, countUserPurchases, userMax
 
-def index(id):
+def delCustomer(id):
+    mongo = current_app.config['MONGO']
+    page = int(request.args.get('page', 1))
+    limit = 20
+    skip = (page - 1) * limit 
+    collection = mongo.db.users 
+
+    total_records = collection.count_documents({})
+    total_pages = (total_records // limit) + (1 if total_records % limit > 0 else 0)
+    data = list(collection.find().skip(skip).limit(limit))
+
+    if id:
+        collection.delete_one({"_id": ObjectId(id)})
+    #----------------------
+    return render_template('customer.html', records=data, page=page, total_pages=total_pages, totalUser=countUser(collection), totalPurchases=countUserPurchases(collection), user=userMax(collection))
+
+def delProduct(id):
     #Không được xoá đoạn duới này, đoạn dưới là code để chia thành các trang
     mongo = current_app.config['MONGO']
     page = int(request.args.get('page', 1))
@@ -16,5 +33,4 @@ def index(id):
     if id:
         collection.delete_one({"_id": ObjectId(id)})
     #----------------------
-
-    return redirect(url_for('product.home_route')) 
+    return render_template('product.html', records=data, page=page, total_pages=total_pages, totalUser=countUser(collection), totalPurchases=countUserPurchases(collection), user=userMax(collection))
--- blueprint/delete/route.py
+++ blueprint/delete/route.py
@@ -1,7 +1,11 @@
 from . import delete_bp
-from .delete_controller import index
+from .delete_controller import delCustomer, delProduct
 
+@delete_bp.route('/customer/delete/<string:id>', methods=['POST'])
+def viewCustomer(id):
+    return delCustomer(id)
 
 @delete_bp.route('/product/delete/<string:id>', methods=['POST'])
-def home_route(id):
-    return index(id)
+def viewProduct(id):
+    return delProduct(id)
+
