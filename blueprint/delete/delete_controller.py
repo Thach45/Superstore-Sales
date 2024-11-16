f428d1cd9d33ac19d701b311621b8954e56f7b29
anhhiii <thutrangyuri510@gmail.com>
2024-11-17 01:59:28 +0700

    Add delete function for Order

--- blueprint/delete/delete_controller.py
+++ blueprint/delete/delete_controller.py
@@ -16,7 +16,8 @@ def delCustomer(id):
     if id:
         collection.delete_one({"_id": ObjectId(id)})
     #----------------------
-    return render_template('customer.html', records=data, page=page, total_pages=total_pages, totalUser=countUser(collection), totalPurchases=countUserPurchases(collection), user=userMax(collection))
+    return redirect(url_for('customer.home_route', page=page))
+    # return render_template('customer.html', records=data, page=page, total_pages=total_pages, totalUser=countUser(collection), totalPurchases=countUserPurchases(collection), user=userMax(collection))
 
 def delProduct(id):
     #Không được xoá đoạn duới này, đoạn dưới là code để chia thành các trang
@@ -33,4 +34,20 @@ def delProduct(id):
     if id:
         collection.delete_one({"_id": ObjectId(id)})
     #----------------------
-    return render_template('product.html', records=data, page=page, total_pages=total_pages, totalUser=countUser(collection), totalPurchases=countUserPurchases(collection), user=userMax(collection))
+    return redirect(url_for('product.home_route', page=page))
+
+
+def delOrder(id):
+    #code phan chia thanh cac trang
+    mongo = current_app.config['MONGO']
+    page = int(request.args.get('page', 1))
+    limit = 20
+    skip = (page - 1) * limit
+    collection = mongo.db.orders
+    total_records = collection.count_documents({})
+    total_pages = (total_records // limit) + (1 if total_records % limit > 0 else 0)
+    data = list(collection.find().skip(skip).limit(limit))
+    #No delete code-------
+    if id:
+        collection.delete_one({"_id": ObjectId(id)})
+    return redirect(url_for('order.home_route', page=page))
