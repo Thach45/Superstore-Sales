558b53ac0378f33aba7b89dd1697f226ac141352
anhhiii <thutrangyuri510@gmail.com>
2024-11-18 23:23:08 +0700

    Add filter function for orders

--- blueprint/filter/route.py
+++ blueprint/filter/route.py
@@ -1,5 +1,9 @@
 from . import filter_bp
-from .filter_controller import home
+from .filter_controller import home, filter_orders
 @filter_bp.route('/customer/filter',methods=["GET"])
 def searchHome():
     return home()
+
+@filter_bp.route('/order/filter',methods=["GET"])
+def filterOrder():
+    return filter_orders()
--- static/js/order.js
+++ static/js/order.js
@@ -53,14 +53,21 @@ if (sortFre) {
     });
 }
 //chức năng filter
+
 if (applyFilter) {
     applyFilter.addEventListener('click', function () {
-        let ship = filterShip.value;
-        let order = filterOrder.value;
-        window.location.href = '/order/filter?Shipper=' + ship + '&Order=' + order;
-       
+        let ship = document.querySelector('.shipFilter').value;
+        let order = document.querySelector('.orderFilter').value;
+        
+        // Kiểm tra nếu người dùng đã chọn tháng và năm
+        if (order && ship) {
+            console.log("Order Month: ", order);  // Xem giá trị OrderMonth
+            console.log("Ship Month: ", ship);  // Xem giá trị ShipMonth
+            window.location.href = '/order/filter?ShipMonth=' + ship + '&OrderMonth=' + order;
+        }
     });
 }
+
 
 document.addEventListener('DOMContentLoaded', function () {
     let paginationLinks = document.querySelectorAll('.pagination .page-link');
