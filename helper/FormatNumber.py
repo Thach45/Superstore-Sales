4be2ca014ef97bc60b941361b939f824f62a442c
tm-Quan745 <tmquan.thpt.pvd2023@gmail.com>
2024-11-19 22:41:30 +0700

    add helper.FormatNuber

---
+++ helper/FormatNumber.py
@@ -0,0 +1,28 @@
+def format_number(number, decimal_places=2):
+    # Nếu input là số, đảm bảo là chuỗi trước khi loại bỏ dấu phẩy
+    if isinstance(number, (int, float)):
+        number_str = str(number) 
+    else:
+        # Nếu đầu vào là chuỗi, kiểm tra có dấu phẩy hay không
+        number_str = number.replace(",", "")  
+
+    # Chuyển chuỗi đã chuẩn hóa thành float
+    number = float(number_str)
+
+    # Trả về chuỗi đã được định dạng
+    return f"{number:,.{decimal_places}f}"
+
+def format_number1(number, decimal_places=2):
+    # Nếu input là số, đảm bảo là chuỗi trước khi loại bỏ dấu phẩy
+    if isinstance(number, (int)):
+        number_str = str(number) 
+    else:
+        # Nếu đầu vào là chuỗi, kiểm tra có dấu phẩy hay không
+        number_str = number.replace(",", "")  
+
+    # Chuyển chuỗi đã chuẩn hóa thành float
+    number = int(number_str)
+
+    # Trả về chuỗi đã được định dạng
+    return f"{number}"
+
