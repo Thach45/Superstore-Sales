cb5a525e09159affda752530fd6596a7707f98a2
hoangthach.dev <nguyenhoangthach2005@gmail.com>
2024-11-29 15:45:40 +0700

    add default route

--- blueprint/home/route.py
+++ blueprint/home/route.py
@@ -1,6 +1,10 @@
-
+from flask import redirect, url_for
 from . import home_bp
 from .home_controller import home
+
 @home_bp.route('/home')
 def home_route():
     return home()
+@home_bp.route('/')
+def default_route():
+    return redirect(url_for('home.home_route'))
